bd43c937dd77e2046891374be45358b7adef977e
RaulGS <119356579+Wisiwy@users.noreply.github.com>
2023-11-07 11:19:07 +0100

    Cla_Arrays

--- cla6_funcionesFlechas/funciones.php
+++ cla6_funcionesFlechas/funciones.php
@@ -1,39 +1,18 @@
 <?php
+/*$nombre = "Raul";
+$array = [1, 2, 3, 4, 5, 6];
+var_dump($array); //vuelca en memoria la variable. Debug. Ver cuanto vale una variable. Son usadas.
+//Instalar xdebug sudo apt update && sudo
 
-/*Funciones anónimas y funciones flecha*/
+//FUNCIONES
+//si me llega una variable (leer formularios
 
-$pre = "Qué tal estas";
-function saludo ($msj){
-    /*Acceder a variable fuera de la función*/
-    global $pre;
-    /*Volvemos global una variable interna de la función*/
-    global $msj;
-    $msj = $msj." mas texto que pongon en la función";
-    echo $pre.$msj;
-
+if (empty($valor)) {
+    echo empty($valor) ? //true si variable vacia, 0 o false devuelve vacia.
+        $valor = 0;
+    echo empty($valor)?
+echo empty($valor) ?
+echo empty($valor)  ;
 }
-/*Referenciamos la variable
-     &pre->Le pasamos la posicíón de memoria de la variable - Pasmos por valor
-Pasar por valor o por referencia. */
 
-function saludo2 ($msj, &:$pre){
-    global $pre;
-    global $msj;
-    $msj = $msj." mas texto que pongon en la función";
-    echo $pre.$msj;
-
-}
-$msj = "Pedro";
-
-/*------------------------------------------*/
-/*Use accedemos valirames del ambito del script. Con &$pre -> referenciamos la dirección de memoria-*/
-$saludo = function ($msj) use (&$pre){
-    return{$pre.$msj};
-};
-
-/*Funciones flecha
-    Tengo acceso atodas las variables del codigo pero sin poder modificar. */
-/*$saludo = fn() => expresion;*/
-$saludo = fn($msj) => $msj;/*A partir de la version 7.3*/
-echo $saludo("qué tal estas");
-exit();
+*/?>
