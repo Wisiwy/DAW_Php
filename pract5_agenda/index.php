2bee71b0b59736c3c108fa3c1e3223e31acc5ae0
RaulGS <119356579+Wisiwy@users.noreply.github.com>
2023-11-16 11:15:05 +0100

    Agenda v0. Medio arreglada

--- pract5_agenda/index.php
+++ pract5_agenda/index.php
@@ -1,48 +1,55 @@
 <?php
-$cuentaContactos = 0;
 $tablaContactos = 'Agenda sin contactos'; //Tabla a mostrar
-$agenda = $_POST['agenda'] ?? [];
-
+$cuentaContactos = 0;
 if (isset($_POST['submit'])) {
+    $cuentaContactos = $_POST['cuenta'];
+    //RECOGER VARIABLES
+    $agenda = $_POST['agenda'] ?? [];
+    $nombre = htmlspecialchars(filter_input(INPUT_POST, 'nombre')) ?? null;
+    $telefono = filter_input(INPUT_POST, 'telefono') ?? 0;
     /*$nombre = $_POST['nombre'];
     $telefono = $_POST['telefono'];*/
-    $nombre = htmlspecialchars(filter_input(INPUT_POST, 'nombre'));
-    $telefono = filter_input(INPUT_POST, 'telefono');
-
-    $error = valida_datos($agenda, $nombre, $telefono);
+    $error = validaDatos($agenda, $nombre, $telefono);
+    echo "Error ".$error;
     if ($error == null) {
         //agrega, borra o actualiza depende del error
-        $agenda[$nombre] = $telefono; //Agrega tlf si esta bien
-
-    }
+        $agenda[$nombre] = $telefono;//Agrega tlf si esta bien
+        $cuentaContactos++;
+        $msjError = '';
+    } else {
         switch ($error) {
             case 1: //nombre esta vacio
                 $msjError = "Añade nombre";
+                break;
             case 2: //telefono no es numerico
                 $msjError = "Añade teléfono valido. ";
+                break;
             case 3: //nombre ya existe en la agenda
                 $msjError = "Contacto ya existe. Intro otro.";
+                break;
+            default:
+                $msjError = "tu puta madre";
         }
 
-    $cuentaContactos++;
-
-    /*Escribre tabla*/
-
-    $tablaContactos = "<h3>Nombre: $nombre, Telefono: $telefono</h3>";
-
-
-    function valida_datos($agenda, $nombre, $telefono)
-    {
-        $error = null;
-        if ($nombre == '')
-            $error = 1;
-        if (!(is_numeric($telefono)))
-            $error = 2;
-        if ((isset($agenda[$nombre]))
-        $error = 3;
-        return $error;
-
     }
+    /*Escribe tabla*/
+    foreach ($agenda as $nombre => $telefono) {
+        if ($telefono != null)
+            $tablaContactos .= "<h3>Nombre: $nombre, Telefono: $telefono</h3>";
+    }
+}
+if (isset($_POST['borrar'])) {
+    unset($agenda);
+}
+function validaDatos($agenda, $nombre, $telefono): int|null
+{
+    if ($nombre == null)
+        return 1;
+    if (!(is_numeric($telefono)))
+        return 2;
+    if ((isset($agenda[$nombre])))
+        return 3;
+    return null;
 }
 
 ?>
@@ -62,13 +69,22 @@ if (isset($_POST['submit'])) {
 <form action="index.php" method="post">
     <fieldset>
         <legend><em>Nuevo Contacto</em></legend>
+        <h2><span style="color: red"><?= $msjError ?></span></h2>
         <label for="nombre">Nombre</label>
         <input type="text" name="nombre" id=""><br>
         <label for="telefono">Teléfono</label>
         <input type="text" name="telefono" id=""><br>
-
         <input type="submit" value="Actualizar agenda" name="submit">
-        <input type="submit" value="Eliminar contacto" name="submit">
+        <input type="submit" value="Eliminar contacto" name="borrar">
+        <input type="hidden" value="<?= $cuentaContactos ?>" name="cuenta">
+        <!--Pasamos valores-->
+        <?php
+        foreach ($agenda as $nombre => $telefono) {
+            echo "<input type='hidden' value  ='$telefono' name='agenda[$nombre]'>";
+        }
+
+        ?>
+
     </fieldset>
 </form>
 <!--Listado de contactos-->
